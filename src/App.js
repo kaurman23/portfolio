836345ee101d61666c329648a49000293910b00e
Manpreet Kaur <kaurman2305@gmail.com>
2020-07-15 02:30:34 +0530

    Add Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./App.css";
 import {NavBar} from "./components/NavBar";
 
 function App() {
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,22 +2,34 @@ import React from 'react'
 
 export const NavBar = () => {
     return (
-        <div className="container-fluid">
-            <nav className="navbar navbar-dark navbar-expand row">
-                <a className="navbar-brand">Home</a>
-                <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                        <a className="nav-link">Home</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link">Projects</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link">More</a>
-                    </li>
-                </ul>
-            </nav>
+        <div className="container-fluid " id="navbar">
+            <nav className="navbar navbar-expand-lg navbar-dark">
+            <a className="navbar-brand">Home</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
             
+                    <ul className="navbar-nav ml-auto ">
+                        <li className="nav-item ">
+                            <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item ">
+                            <a className="nav-link" href="#">About Me</a>
+                        </li>
+                        <li className="nav-item ">
+                            <a className="nav-link" href="#">Projects</a>
+                        </li>
+                        <li className="nav-item ">
+                            <a className="nav-link" href="#">More</a>
+                        </li>
+                        <li className="nav-item ">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                        
+                    </ul>
+            </div>
+            </nav>            
         </div>
     )
 }
