142356e4fed30edd9826f941504b75e6c4e624e1
Manpreet Kaur <kaurman2305@gmail.com>
2020-07-16 14:32:52 +0530

    Add About Me section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import "./App.css";
 import {NavBar} from "./components/NavBar";
 import {Main} from './components/Main';
+import {AboutMe} from "./components/AboutMe"
 
 function App() {
   return (
     <div >
       <NavBar />
       <Main />
+      <AboutMe />
     </div>
   );
 }
---
+++ src/components/AboutMe.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const AboutMe = () => {
+    return (
+        <div className="container-fluid about-me">
+            <div className="row t">
+                
+                <div className="col-12 col-md-4 about-me-content">
+                    <img src="/assets/images/dp.svg" />
+                </div>
+                <div className="col-12 col-md-6 about-me-content">
+                    <h1 className=" about-me-title">
+                        GET TO KNOW ME!
+                    </h1>
+                    <p className=" ">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                </div>
+            </div>
+        </div>
+    )
+}
